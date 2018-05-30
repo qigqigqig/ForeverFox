1a2d5e789349e51fdbb7a0b938888b78f58862b4
qigqigqig <39426627+qigqigqig@users.noreply.github.com>
2018-05-30 01:38:45 -0400

    Music Build

---
+++ Assets/spawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnManager : MonoBehaviour {
+	public int spawn = 0;
+	public GameObject spawner;
+	// Use this for initialization
+	void Start () {
+		spawner = GameObject.Find("FireFlySpawner");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+}
