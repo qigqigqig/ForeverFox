1ea8a42d9079396269217d6c0de18c8b72b59fb7
qigqigqig <39426627+qigqigqig@users.noreply.github.com>
2018-06-25 01:05:28 -0400

    Clouds

---
+++ Assets/Scripts/cloudControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudControl : MonoBehaviour {
+    public Sprite[] theClouds;
+    private SpriteRenderer spriteRenderer;
+    private int randomNum;
+    private Vector3 startPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        randomNum = UnityEngine.Random.Range(0, 2);
+        startPos = transform.position;
+        startPos.y = -1.3f;
+        spriteRenderer.sprite = theClouds[randomNum];
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
