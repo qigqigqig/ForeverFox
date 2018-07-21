9246f45c7da52584311b9031f486ef331de65960
qigqigqig <39426627+qigqigqig@users.noreply.github.com>
2018-07-21 18:00:43 -0400

    Spawn Changer

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -16,22 +16,23 @@ public class gameManager : MonoBehaviour {
     public bool beachSpawned;
     public bool desertSpawned;
     public bool greekSpawned;
-	public GameObject Chicken;
+    public GameObject Chicken;
     public bool forestSpawned;
     public string genSpawn;
-	public string deathCause;
+    public string deathCause;
     public GameObject[] mountains;
     public GameObject[] fEnemies;
-	public int chickenCounter = 1;
+    public int chickenCounter = 1;
     public GameObject beachSpawner;
     public GameObject theCamera;
     public GameObject desertSpawner;
     private int xPos;
+    private bool genWorlds = true;
     public GameObject greekSpawner;
     private int skyCounter = 0;
     private int mountainCounter = 0;
-    private int skyCounter2 = 0;
-    private int mountainCounter2 = 0;
+    public int skyCounter2 = 0;
+    public int mountainCounter2 = 0;
     public GameObject forestSpawner;
     public bool isDead = false;
     private int eCounter = 0;
@@ -57,43 +58,46 @@ public class gameManager : MonoBehaviour {
     public int[] randomNumbers;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         genSpawn = "Forest";
         generateWorld();
         generateSky();
-	//	generateChickens();
-       // generateMountain();
-       // spawnEnemies();
+        //	generateChickens();
+        // generateMountain();
+        // spawnEnemies();
         beachSpawned = false;
         desertSpawned = false;
         playerStartPoint = thePlayer.transform.position;
         startBox = thePlayer.myCollider;
         sizeDefaults = thePlayer.GetComponent<BoxCollider2D>().size;
-       // beachSpawner = GameObject.Find("beachSpawnPoint");
-      //  desertSpawner = GameObject.Find("desertSpawnPoint");
-       // greekSpawner = GameObject.Find("greekSpawnPoint");
+        // beachSpawner = GameObject.Find("beachSpawnPoint");
+        //  desertSpawner = GameObject.Find("desertSpawnPoint");
+        // greekSpawner = GameObject.Find("greekSpawnPoint");
         //forestSpawner = GameObject.Find("forestSpawnPoint");
-        
+
     }
 
     // Update is called once per frame
     void Update() {
-       // Debug.Log("Player Position" + thePlayer.transform.position.x);
-        xPos = (int)Math.Floor(thePlayer.transform.position.x);
-   //    Debug.Log("Player Position UPDATED woo:" + xPos);
+        // Debug.Log("Player Position" + thePlayer.transform.position.x);
+      //  xPos = (int)Math.Floor(thePlayer.transform.position.x);
+        //    Debug.Log("Player Position UPDATED woo:" + xPos);
 
-  //      timeCounter = timeCounter + 1;
-        if (xPos % 750 == 0)
-        {
-     //       Debug.Log("Generating More Worlds.");
-            mountainCounter2 = 0;
-            skyCounter2 = 0;
-            generateWorld();
-            generateSky();
-			//generateChickens();
-         //   generateMountain();
-            xPos = xPos + 1;
-        }
+        //      timeCounter = timeCounter + 1;
+        //Debug.Log(xPos);
+        //if (xPos % 750 == 0)
+        //{
+            //Debug.Log("Generating More Worlds.");
+            //  Debug.Log(xPos);
+
+            //mountainCounter2 = 0;
+            //skyCounter2 = 0;
+           // generateWorld();
+         //   generateSky();
+            //generateChickens();
+            //   generateMountain();
+            //xPos = xPos + 1;
+       // }
         if (thePlayer.transform.position.x >= 0)
         {
             isDead = false;
@@ -101,38 +105,38 @@ public class gameManager : MonoBehaviour {
 
         }
         //beachSpawner = GameObject.Find("beachSpawnPoint");
-      //  desertSpawner = GameObject.Find("desertSpawnPoint");
-       // greekSpawner = GameObject.Find("greekSpawnPoint");
+        //  desertSpawner = GameObject.Find("desertSpawnPoint");
+        // greekSpawner = GameObject.Find("greekSpawnPoint");
         //forestSpawner = GameObject.Find("forestSpawnPoint");
-      /*  if (thePlayer.transform.position.x > beachSpawner.transform.position.x && beachSpawned == false && beachSpawner !=null)
-        {
-            beachSpawned = true;
-            genSpawn = "Beach";
-            generateWorld();
+        /*  if (thePlayer.transform.position.x > beachSpawner.transform.position.x && beachSpawned == false && beachSpawner !=null)
+          {
+              beachSpawned = true;
+              genSpawn = "Beach";
+              generateWorld();
 
-        }
-        if (thePlayer.transform.position.x > desertSpawner.transform.position.x && desertSpawned == false && desertSpawner != null)
-        {
-            desertSpawned = true;
-            genSpawn = "Desert";
-            generateWorld();
+          }
+          if (thePlayer.transform.position.x > desertSpawner.transform.position.x && desertSpawned == false && desertSpawner != null)
+          {
+              desertSpawned = true;
+              genSpawn = "Desert";
+              generateWorld();
 
-        }
-        if (thePlayer.transform.position.x > greekSpawner.transform.position.x && greekSpawned == false && greekSpawner != null)
-        {
-            greekSpawned = true;
-            genSpawn = "Greek";
-            generateWorld();
+          }
+          if (thePlayer.transform.position.x > greekSpawner.transform.position.x && greekSpawned == false && greekSpawner != null)
+          {
+              greekSpawned = true;
+              genSpawn = "Greek";
+              generateWorld();
 
-        }
-        if (thePlayer.transform.position.x > forestSpawner.transform.position.x && forestSpawned == false && forestSpawner != null)
-        {
-            forestSpawned = true;
-            genSpawn = "Forest";
-            generateWorld();
+          }
+          if (thePlayer.transform.position.x > forestSpawner.transform.position.x && forestSpawned == false && forestSpawner != null)
+          {
+              forestSpawned = true;
+              genSpawn = "Forest";
+              generateWorld();
 
-        }*/
-        
+          }*/
+
         /*  if (thePlayer.transform.position.x > desertSpawner.transform.position.x && desertSpawned == false)
           {
               eCounter = 0;
@@ -323,12 +327,12 @@ public class gameManager : MonoBehaviour {
 
      }
      */
-     public void generateSky()
+    public void generateSky()
     {
-      //  Debug.Log("IS sky being generated???");
+        //  Debug.Log("IS sky being generated???");
         while (skyCounter2 <= 75)
         {
-        //    Debug.Log("IS sky being generated???");
+            //    Debug.Log("IS sky being generated???");
             if (skyCounter == 0)
             {
                 Instantiate(sky, new Vector3(17.58f, -1.32f, 0), Quaternion.identity);
@@ -343,24 +347,24 @@ public class gameManager : MonoBehaviour {
 
         }
     }
-	public void generateChickens()
-	{
-		while (chickenCounter <= 13) {
+    public void generateChickens()
+    {
+        while (chickenCounter <= 13) {
 
-			Instantiate(Chicken, new Vector3((UnityEngine.Random.Range(100,150) * chickenCounter ) , 3), Quaternion.identity);
-			chickenCounter = chickenCounter + 1;
-		}
+            Instantiate(Chicken, new Vector3((UnityEngine.Random.Range(100, 150) * chickenCounter), 3), Quaternion.identity);
+            chickenCounter = chickenCounter + 1;
+        }
 
-	}
+    }
     public void generateMountain()
     {
-       // Debug.Log("IS sky being generated???");
+        // Debug.Log("IS sky being generated???");
         while (mountainCounter2 <= 75)
         {
-         //   Debug.Log("IS sky being generated???");
+            //   Debug.Log("IS sky being generated???");
             if (mountainCounter == 0)
             {
-                Instantiate(mountains[UnityEngine.Random.Range(0,5)], new Vector3(17.58f, -0.9f, 0), Quaternion.identity);
+                Instantiate(mountains[UnityEngine.Random.Range(0, 5)], new Vector3(17.58f, -0.9f, 0), Quaternion.identity);
             }
             else
             {
@@ -371,6 +375,150 @@ public class gameManager : MonoBehaviour {
             mountainCounter2 = mountainCounter2 + 1;
 
         }
+    }
+    public void generateForest()
+    {
+
+        while (counter < 5)
+        {
+            //   Debug.Log("Counter: " + counter);
+            randomNumbers[counter] = UnityEngine.Random.Range(0, 10);
+            if (counter == 0 && fullLoop == 0)
+            {
+
+                fGen[0] = Instantiate(fDefault[0], new Vector3(17.58f, 0.52f, 0), Quaternion.identity);
+            }
+            if (counter == 0 && fullLoop == 1)
+            {
+
+                //fGen[0] = Instantiate(fDefault[0], new Vector3(17.58f, 0.52f, 0), Quaternion.identity);
+                fGen[0] = Instantiate(fDefault[randomNumbers[counter]], gGen[3].transform.position + (gGen[3].transform.right * (gGen[3].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2 + fDefault[randomNumbers[counter]].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), gGen[3].transform.rotation);
+
+            }
+            if (counter >= 1 && counter != 4)
+            {
+                fGen[counter] = Instantiate(fDefault[randomNumbers[counter]], fGen[counter - 1].transform.position + (fGen[counter - 1].transform.right * (fGen[counter - 1].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2 + fDefault[randomNumbers[counter]].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), fDefault[counter - 1].transform.rotation);
+
+
+            }
+            if (counter == 4)
+            {
+                fGen[counter] = Instantiate(fDefault[11], fGen[counter - 1].transform.position + (fGen[counter - 1].transform.right * ((fGen[counter - 1].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2) - 1 + fDefault[11].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), fDefault[counter - 1].transform.rotation);
+                beachSpawner = GameObject.Find("beachSpawnpoint");
+
+
+
+            }
+            counter = counter + 1;
+
+        }
+        counter = 0;
+
+    }
+    public void generateBeach()
+    {
+        while (counter < 4)
+        {
+            randomNumbers[counter] = UnityEngine.Random.Range(0, 4);
+            // Debug.Log("Testing:" + counter + beachScenes[counter]);
+            if (counter == 0)
+            {
+                //  Debug.Log("Testing: " + forest[11].transform.position.x);
+                bGen[0] = Instantiate(bDefault[randomNumbers[counter]], fGen[4].transform.position + (fGen[4].transform.right * (fGen[4].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2 + bDefault[randomNumbers[counter]].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), fGen[4].transform.rotation);
+
+
+
+            }
+            if (counter >= 1 && counter < 3)
+            {
+                bGen[counter] = Instantiate(bDefault[randomNumbers[counter]], bGen[counter - 1].transform.position + (bGen[counter - 1].transform.right * (bGen[counter - 1].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2 + bDefault[randomNumbers[counter]].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), bDefault[counter - 1].transform.rotation);
+
+
+            }
+            if (counter == 3)
+            {
+                bGen[counter] = Instantiate(bDefault[4], bGen[counter - 1].transform.position + (bGen[counter - 1].transform.right * (bGen[counter - 1].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2 + bDefault[4].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), bDefault[counter - 1].transform.rotation);
+                desertSpawner = GameObject.Find("desertSpawnPoint");
+
+            }
+            counter = counter + 1;
+
+        }
+        counter = 0;
+
+    }
+    public void generateDesert()
+    {
+        while (counter < 4)
+        {
+            randomNumbers[counter] = UnityEngine.Random.Range(0, 4);
+            // Debug.Log("Testing:" + counter + beachScenes[counter]);
+            if (counter == 0)
+            {
+                //  Debug.Log("Testing: " + forest[11].transform.position.x);
+                dGen[0] = Instantiate(dDefault[randomNumbers[counter]], bGen[3].transform.position + (bGen[3].transform.right * (bGen[3].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2 + dDefault[randomNumbers[counter]].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), bGen[3].transform.rotation);
+
+
+
+            }
+            if (counter >= 1 && counter < 3)
+            {
+                dGen[counter] = Instantiate(dDefault[randomNumbers[counter]], dGen[counter - 1].transform.position + (dGen[counter - 1].transform.right * (dGen[counter - 1].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2 + dDefault[randomNumbers[counter]].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), dDefault[counter - 1].transform.rotation);
+
+
+            }
+            if (counter == 3)
+            {
+                dGen[counter] = Instantiate(dDefault[4], dGen[counter - 1].transform.position + (dGen[counter - 1].transform.right * (dGen[counter - 1].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2 + dDefault[4].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), dDefault[counter - 1].transform.rotation);
+                greekSpawner = GameObject.Find("greekSpawnPoint");
+
+
+            }
+            counter = counter + 1;
+
+        }
+        counter = 0;
+
+    }
+
+    public void generateGreek()
+    {
+
+        while (counter < 4)
+        {
+            randomNumbers[counter] = UnityEngine.Random.Range(0, 4);
+            // Debug.Log("Testing:" + counter + beachScenes[counter]);
+            if (counter == 0)
+            {
+                //  Debug.Log("Testing: " + forest[11].transform.position.x);
+                gGen[0] = Instantiate(gDefault[randomNumbers[counter]], dGen[3].transform.position + (dGen[3].transform.right * (dGen[3].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2 + gDefault[randomNumbers[counter]].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), dGen[3].transform.rotation);
+
+
+
+            }
+            if (counter >= 1 && counter < 3)
+            {
+                gGen[counter] = Instantiate(gDefault[randomNumbers[counter]], gGen[counter - 1].transform.position + (gGen[counter - 1].transform.right * (gGen[counter - 1].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2 + gDefault[randomNumbers[counter]].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), gDefault[counter - 1].transform.rotation);
+
+
+            }
+            if (counter == 3)
+            {
+                gGen[counter] = Instantiate(gDefault[4], gGen[counter - 1].transform.position + (gGen[counter - 1].transform.right * (gGen[counter - 1].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2 + gDefault[4].gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2)), gDefault[counter - 1].transform.rotation);
+                greekSpawned = false;
+                forestSpawned = false;
+                desertSpawned = false;
+                beachSpawned = false;
+                fullLoop = 1;
+                forestSpawner = GameObject.Find("forestSpawnPoint");
+
+
+            }
+            counter = counter + 1;
+
+        }
+        counter = 0;
+
     }
     public void generateWorld()
     {
